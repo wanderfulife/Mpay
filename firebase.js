f4dd801bc366ee1f7361081e602f6e744fcc2965
wanderfulife <john.wander@hotmail.fr>
2022-07-15 10:47:57 +1000

    chat

--- firebase.js
+++ firebase.js
@@ -5,16 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
 
 
-
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-	
+
 };
 
 
